b65cc67df08363f5ee81580a7afae2a40cd63884
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-24 15:03:23 +0530

    Initial commit

---
+++ weather.js
@@ -0,0 +1,3 @@
+//Complete the Weather API Backend part using openweathermap api
+
+// Progression 1: Create a function and fetch data using "fetch" from openweathermap api and display the data as given in reference image.
